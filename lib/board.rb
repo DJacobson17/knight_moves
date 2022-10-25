fdc1763fdf5f7409529e436a945a5c5c7a3d22a0
DJacobson17 <davidbjacobson@gmail.com>
2022-10-25 13:45:30 -0500

    created a method to display current position and possible moves

--- lib/board.rb
+++ lib/board.rb
@@ -9,7 +9,6 @@ class Board # rubocop:disable Style/Documentation
     array = a.product(a)
     @squares = []
     build_board(array)
-    connect_neighbors
   end
 
   def add_square(name)
@@ -27,46 +26,51 @@ class Board # rubocop:disable Style/Documentation
     squares.length
   end
 
-  def add_edge(start_name, end_name, undirected: true)
-    from = squares.index { |s| s.name == start_name }
-    to   = squares.index { |s| s.name == end_name }
-    # binding.pry
-    squares[from].neighbors[to] = true
-    squares[to].neighbors[from] = true if undirected
+  def add_edge(start_name, end_name)
+    find_square_by_name(start_name).children.push(find_square_by_name(end_name))
   end
 
   def build_board(array)
     array.each { |name| add_square(name) }
   end
 
-  def connect_neighbors
-    @squares.each { |square| calculate_neighbors(square) }
+
+  def calculate_children(square)
+    current_square = square.name.dup
+    current_square[0] -= 1
+    current_square[1] += 2
+    add_edge(square.name, current_square) if find_square_by_name(current_square)
+    current_square[1] -= 4
+    add_edge(square.name, current_square) if find_square_by_name(current_square)
+    current_square[0] -= 1
+    current_square[1] += 1
+    add_edge(square.name, current_square) if find_square_by_name(current_square)
+    current_square[1] += 2
+    add_edge(square.name, current_square) if find_square_by_name(current_square)
+    current_square[0] += 4
+    add_edge(square.name, current_square) if find_square_by_name(current_square)
+    current_square[1] -= 2
+    add_edge(square.name, current_square) if find_square_by_name(current_square)
+    current_square[0] -= 1
+    current_square[1] -= 1
+    add_edge(square.name, current_square) if find_square_by_name(current_square)
+    current_square[1] += 4
+    add_edge(square.name, current_square) if find_square_by_name(current_square)
   end
 
-  def calculate_neighbors(square)
-    array = square.name.dup
-    array[0] -= 1
-    array[1] += 2
-    add_edge(square.name, array) if find_square_by_name(array)
-    array[1] -= 4
-    add_edge(square.name, array) if find_square_by_name(array)
-    array[0] -= 1
-    array[1] += 1
-    add_edge(square.name, array) if find_square_by_name(array)
-    array[1] += 2
-    add_edge(square.name, array) if find_square_by_name(array)
-    array[0] += 4
-    add_edge(square.name, array) if find_square_by_name(array)
-    array[1] -= 2
-    add_edge(square.name, array) if find_square_by_name(array)
-    array[0] -= 1
-    array[1] -= 1
-    add_edge(square.name, array) if find_square_by_name(array)
-    array[1] += 4
-    add_edge(square.name, array) if find_square_by_name(array)
+  def display_moves(square)
+    moves = []
+    calculate_children(square)
+    square.children.each do |child|
+      moves << child.name
+    end
+    puts "You are on space #{square.name}."
+    puts "Your possible moves are: #{moves}."
   end
+
 end
 
 b = Board.new
-s = b.find_square_by_name([1, 4])
-p s
+s = b.find_square_by_name([4, 4])
+b.display_moves(s)
+
--- lib/square.rb
+++ lib/square.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Square # rubocop:disable Style/Documentation
-  attr_accessor :name, :neighbors
+  attr_accessor :name, :children
 
   def initialize(name = [])
     @name = name
-    @neighbors = []
+    @children = []
+    
   end
 end
