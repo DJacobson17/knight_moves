b3496ca2f923000d5f846237ba00b67f58f93067
DJacobson17 <davidbjacobson@gmail.com>
2022-10-28 12:27:33 -0500

    created bfs and traceback method.  Also wrote knight_moves method that displays traceback

--- lib/board.rb
+++ lib/board.rb
@@ -11,19 +11,12 @@ class Board # rubocop:disable Style/Documentation
   end
 
   def find_square_by_name(name)
-    squares.each do |s|
-      return s if s.name == name
-    end
-    nil
-  end
-
-  def count
-    squares.length
+    @squares.find { |s| s.name == name }
   end
 
   def build_board(array)
     result = []
-    array.each do |name| 
+    array.each do |name|
       new_square = Square.new(name)
       new_square.adj = calculate_adjacents(name)
       result << new_square
@@ -46,18 +39,41 @@ class Board # rubocop:disable Style/Documentation
     adj.select { |position| position[0].between?(1, 8) && position[1].between?(1, 8)}
   end
 
+  def knight_moves(start, stop)
+    k = bfs(start, stop)
+    puts "Congrats! You made it in #{k.length - 1} moves!"
+    k.each { |move| p move }
+  end
 
-  def knight_moves(init, dest, result = [])
-    start_node = find_square_by_name(init)
-    end_node = find_square_by_name(dest)
-    return result if init.nil?
-    
-    calculate_children(start_node)
-    start_node.children.each { |child| result << child }
-    knight_moves(child, dest, )
+  def bfs(start, stop)
+    find_square_by_name(start).dist = 0
+    queue = [find_square_by_name(start)]
+    until queue.empty?
+      current_node = queue.shift
+      break if current_node.name == stop
+
+      current_node.adj.each do |adj|
+        square = find_square_by_name(adj)
+        next unless square.dist.nil?
+
+        square.dist = current_node.dist + 1
+        square.pred = current_node
+        queue << square
+      end
+    end
+    display_path(current_node)
+  end
+
+  def display_path(stop)
+    result = []
+    queue = [stop.name]
+    until queue.empty?
+      current_node = find_square_by_name(queue.shift)
+      current_node.pred && queue << current_node.pred.name
+      result.unshift(current_node.name)
+    end
+    result
   end
 
 end
 
-b = Board.new
-p b
