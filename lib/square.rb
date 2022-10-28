da9cac61002d8d29a291e8bbad9799a8ae81ad1c
DJacobson17 <davidbjacobson@gmail.com>
2022-10-28 12:28:47 -0500

    added previous destination and distance to nodes

--- lib/square.rb
+++ lib/square.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class Square # rubocop:disable Style/Documentation
-  attr_accessor :name, :adj
+  attr_accessor :name, :adj, :pred, :dist
 
   def initialize(name = [])
     @name = name
     @adj = []
+    @pred = nil
+    @dist = nil
   end
 end
